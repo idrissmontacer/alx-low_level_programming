1940904b3c724b69e5658937733d392227c6132f
idrissmontacer <idrissmontacer@gmail.com>
2023-10-05 15:12:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point of the program
+*
+* Return: Always 1
+*/
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar\n", 2015 - 10 - 19, stdout);
+return (1);
+}
