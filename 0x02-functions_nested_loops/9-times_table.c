e624e89b0751348e8ac477c205d14780b396358d
idrissmontacer <idrissmontacer@gmail.com>
2023-10-11 13:12:11 -0700

    bc

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,21 +7,21 @@ void times_table(void)
 int i, j, result;
 for (i = 0; i <= 9; i++)
 {
-_putchar(48);
-for (j = 0; j <= 9; j++)
+_putchar('0');
+for (j = 1; j <= 9; j++)
 {
 _putchar(',');
 _putchar(' ');
 result = i * j;
 if (result <= 9)
 {
-_putchar(',');
+_putchar(' ');
 }
 else
 {
-_putchar((result / 10) + 48);
-_putchar((result % 10) + 48);
+_putchar((result / 10) + '0');
 }
+_putchar((result % 10) + '0');
 }
 _putchar('\n');
 }
