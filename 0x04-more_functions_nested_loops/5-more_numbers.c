7b54d2c6df8a6d2bee3302627450604b5933b788
idrissmontacer <idrissmontacer@gmail.com>
2023-10-13 16:58:09 -0700

    gf

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,18 @@ void more_numbers(void)
 {
 int n;
 int m;
+int num;
 for (n = 0; n < 10; n++)
 {
 for (m = 0; m <= 14; m++)
 {
-_putchar(m + '0');
+num = m;
+if (m > 9)
+{
+_putchar(1 + 48);
+num = m % 10;
+}
+_putchar(num + '0');
 }
 _putchar('\n');
 }
